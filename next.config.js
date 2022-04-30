d82fec9097b8741382057e00e70090faa58b2c5b
BrunoCaputo <brunopc.dev@gmail.com>
2022-04-30 09:58:54 -0300

    styles: Added color pallete to a file and made 1rem = 16px

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
---
+++ styles/colors.tsx
@@ -0,0 +1,21 @@
+const primary = "#6200EE";
+const primaryVariant = "#3700B3";
+const secondary = "#03DAC6";
+const secondaryVariant = "#018786";
+const error = "#B00020";
+const white = "#FFFFFF";
+const black = "#000000";
+const background = "grey";
+const shadow = "#363949";
+
+export {
+  primary,
+  primaryVariant,
+  secondary,
+  secondaryVariant,
+  error,
+  white,
+  black,
+  background,
+  shadow,
+};
